c202cba235136072cf7ad9d8a9cec9bd79365956
Isaac True <isaac.true@canonical.com>
2023-02-09 10:30:25 +0100

    debian.polarfire: use debian directory from linux-riscv
    Signed-off-by: Isaac True <isaac.true@canonical.com>

--- debian.polarfire/rules.d/riscv64.mk
+++ debian.polarfire/rules.d/riscv64.mk
@@ -1,7 +1,7 @@
-human_arch	= RISC-V
-build_arch	= riscv
-header_arch	= $(build_arch)
-defconfig	= mpfs_defconfig
+human_arch      = RISC-V
+build_arch      = riscv
+header_arch     = $(build_arch)
+defconfig       = defconfig
 flavours        = generic
 build_image	= Image
 kernel_file	= arch/$(build_arch)/boot/Image
@@ -10,21 +10,14 @@ install_file	= vmlinuz
 vdso		= vdso_install
 no_dumpfile	= true
 
-do_flavour_image_package = false
-do_tools	= false
-do_tools_common	= false
-do_extras_package = false
-do_source_package = false
-do_doc_package	= false
-do_flavour_header_package = false
-do_common_headers_indep = false
-
-# Skip the ABI chek for now until we get a proper derivative kernel up and
-# running.
-skipabi		= true
-
-# Disable building libc-dev until we have the derivative
 do_libc_dev_package = false
+do_extras_package   = true
+do_tools_usbip      = true
+do_tools_cpupower   = true
+do_tools_perf       = true
+do_tools_perf_jvmti = true
+do_tools_bpftool    = true
 
-disable_d_i	= true
-do_zfs		= false
+do_dtbs           = true
+
+skipabi           = true
